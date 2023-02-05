b0b0319ecdc97e12a4a5b92967855166daae5373
Edsonllima <focusti@hotmail.com>
2023-02-05 12:19:40 -0300

    First Commit

---
+++ Introducao/MyFirstProject/src/MainClassOne.java
@@ -0,0 +1,11 @@
+
+public class MainClassOne {
+
+	public static void main(String[] args) {
+		float numero = 0.5f;
+		numero = 10.05f;
+		System.out.println("Valor de número é :" + numero);
+
+	}
+
+}
